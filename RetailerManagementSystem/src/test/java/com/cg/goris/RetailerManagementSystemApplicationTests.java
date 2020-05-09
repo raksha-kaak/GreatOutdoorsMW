20391ce16d268985dfb1d1f4fdcb6a50f46e67b7
raksha-kaak <ridhika.kak2011@gmail.com>
2020-05-09 14:55:05 +0530

    first commit

---
+++ RetailerManagementSystem/src/test/java/com/cg/goris/RetailerManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.goris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailerManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
